56eda2d0ea16bf8afbadc229dbff714a7cc23731
OoOmarsala7 <omarsalah087@gmail.com>
2023-08-29 10:36:04 +0300

    dfgh

--- script.groovy
+++ script.groovy
@@ -1,27 +1,29 @@
 def buildApp() {
-    echo 'building the appliction'
-     when {
-                expression {
-                    params.versionChoice == '1.0'
-                }
+    echo 'building the application'
+
+    stage('build') {
+        when {
+            expression {
+                params.versionChoice == '1.0'
             }
-            steps {
-                script {
-                    echo "${params.versionChoice} the application..."
-                }
+        }
+        steps {
+            script {
+                echo "${params.versionChoice} the application..."
             }
+        }
+    }
 }
 
 def testingApp() {
     echo "Testing the application..."
 }
 
-
-
 def deployApp() {
     echo "Deploying the application..."
     withCredentials([usernamePassword(credentialsId: 'slhfs', usernameVariable: 'USER', passwordVariable: 'PWD')]) {
-    echo "some script ${USER} ${PWD}"
-}}
+        echo "some script ${USER} ${PWD}"
+    }
+}
 
 return this
